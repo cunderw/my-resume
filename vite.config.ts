783353c054be6be88dc68c1cb015869a328c932e
Carson Underwood <cunderw@gmail.com>
2024-02-04 13:17:48 -0600

    fix vite base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), eslintPlugin(), viteSvgr(), tsconfigPaths()],
-  base: '/my-resume/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
